5d3051a732a1668404462d37797eab82d923eada
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-07-17 00:00:12 +0900

    antd 캘린더 커밋 완료

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,13 +1,28 @@
-import { Calendar } from "antd";
+import { Calendar, Button } from "antd";
 import type { Dayjs } from "dayjs";
 import type { CalendarMode } from "antd/es/calendar/generateCalendar";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
   const onPanelChange = (value: Dayjs, mode: CalendarMode) => {
     console.log(value.format("YYYY-MM-DD"), mode);
   };
 
-  return <Calendar onPanelChange={onPanelChange} />;
+  const goToCreate = () => {
+    navigate("/create");
+  };
+
+  return (
+    <div>
+      <Button type="primary" onClick={goToCreate}>
+        할 일 적기
+      </Button>
+      <Button>테이블로 보기</Button>
+      <Calendar onPanelChange={onPanelChange} />
+    </div>
+  );
 };
 
 export default Home;
