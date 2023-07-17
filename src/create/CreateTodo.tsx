50d2cb525f21f5277176039f41d6862976fe568a
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-07-17 23:24:05 +0900

    할일 적는 모듈 생성 중

--- src/create/CreateTodo.tsx
+++ src/create/CreateTodo.tsx
@@ -1,7 +1,27 @@
 import React from "react";
+import type { DatePickerProps } from "antd";
+import { DatePicker, Space, Input, Form } from "antd";
+import "../css/CreateTodo.css";
 
 const CreateTodo = () => {
-  return <div>hi</div>;
+  const { TextArea } = Input;
+  const onChange: DatePickerProps["onChange"] = (date, dateString) => {
+    console.log(date, dateString);
+  };
+
+  return (
+    <>
+      <Space direction="vertical">
+        <DatePicker onChange={onChange} />
+
+        <div className="textTodo">
+          <Form.Item label="내용">
+            <TextArea rows={4} />
+          </Form.Item>
+        </div>
+      </Space>
+    </>
+  );
 };
 
 export default CreateTodo;
