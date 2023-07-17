f443fc1db100cadc5a83aeb8d157740000f7c3d2
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-07-17 23:23:18 +0900

    calendarHome 모듈화

---
+++ src/calendar/CalendarHome.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { Calendar, Badge } from "antd";
+import type { Dayjs } from "dayjs";
+import type { CalendarMode } from "antd/es/calendar/generateCalendar";
+import type { BadgeProps } from "antd";
+import type { CellRenderInfo } from "rc-picker/lib/interface";
+
+const CalendarHome = () => {
+  const onPanelChange = (value: Dayjs, mode: CalendarMode) => {
+    console.log(value.format("YYYY-MM-DD"), mode);
+  };
+
+  const getListData = (value: Dayjs) => {
+    let listData;
+    switch (value.date()) {
+      case 8:
+        listData = [
+          { type: "warning", content: "This is warning event." },
+          { type: "success", content: "This is usual event." },
+        ];
+        break;
+      case 10:
+        listData = [
+          { type: "warning", content: "This is warning event." },
+          { type: "success", content: "This is usual event." },
+          { type: "error", content: "This is error event." },
+        ];
+        break;
+      case 15:
+        listData = [
+          { type: "warning", content: "This is warning event" },
+          { type: "success", content: "This is very long usual event。。...." },
+          { type: "error", content: "This is error event 1." },
+          { type: "error", content: "This is error event 2." },
+          { type: "error", content: "This is error event 3." },
+          { type: "error", content: "This is error event 4." },
+        ];
+        break;
+      default:
+    }
+    return listData || [];
+  };
+
+  const getMonthData = (value: Dayjs) => {
+    if (value.month() === 8) {
+      return 1394;
+    }
+  };
+
+  const monthCellRender = (value: Dayjs) => {
+    const num = getMonthData(value);
+    return num ? (
+      <div className="notes-month">
+        <section>{num}</section>
+        <span>Backlog number</span>
+      </div>
+    ) : null;
+  };
+
+  const dateCellRender = (value: Dayjs) => {
+    const listData = getListData(value);
+    return (
+      <ul className="events">
+        {listData.map((item) => (
+          <li key={item.content}>
+            <Badge status={item.type as BadgeProps["status"]} text={item.content} />
+          </li>
+        ))}
+      </ul>
+    );
+  };
+
+  const cellRender = (current: Dayjs, info: CellRenderInfo<Dayjs>) => {
+    if (info.type === "date") return dateCellRender(current);
+    if (info.type === "month") return monthCellRender(current);
+    return info.originNode;
+  };
+
+  return <Calendar cellRender={cellRender} />;
+};
+
+export default CalendarHome;
