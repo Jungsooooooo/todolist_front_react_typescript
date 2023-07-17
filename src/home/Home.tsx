ed3c35d192850fb5d89016106be5ae0454613dac
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-07-17 23:23:51 +0900

    calendarHome 모듈 Home에 적용

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,14 +1,11 @@
-import { Calendar, Button } from "antd";
-import type { Dayjs } from "dayjs";
-import type { CalendarMode } from "antd/es/calendar/generateCalendar";
+import { Button } from "antd";
+
+import CalendarHome from "../calendar/CalendarHome";
+
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const navigate = useNavigate();
-
-  const onPanelChange = (value: Dayjs, mode: CalendarMode) => {
-    console.log(value.format("YYYY-MM-DD"), mode);
-  };
 
   const goToCreate = () => {
     navigate("/create");
@@ -20,7 +17,7 @@ const Home = () => {
         할 일 적기
       </Button>
       <Button>테이블로 보기</Button>
-      <Calendar onPanelChange={onPanelChange} />
+      <CalendarHome />
     </div>
   );
 };
