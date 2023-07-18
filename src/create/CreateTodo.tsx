4b25c5b5f1d8d091bea462c420b949f7ccdfd5bd
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-07-18 22:40:58 +0900

    할일 적기 css 정리 및 날짜 내용 부분 생성

--- src/create/CreateTodo.tsx
+++ src/create/CreateTodo.tsx
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import type { DatePickerProps } from "antd";
 import { DatePicker, Space, Input, Form } from "antd";
 import "../css/CreateTodo.css";
 
 const CreateTodo = () => {
   const { TextArea } = Input;
+
+  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+
   const onChange: DatePickerProps["onChange"] = (date, dateString) => {
-    console.log(date, dateString);
+    console.log(date);
   };
 
   return (
     <>
+      <div className="selectDate">
+        <Space direction="vertical">
+          <Form.Item label="날짜">
+            <DatePicker onChange={onChange} />
+          </Form.Item>
+        </Space>
+      </div>
       <Space direction="vertical">
-        <DatePicker onChange={onChange} />
-
         <div className="textTodo">
           <Form.Item label="내용">
             <TextArea rows={4} />
