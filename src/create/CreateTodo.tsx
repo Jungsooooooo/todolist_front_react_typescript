ee2b97709f7e1074f96eb5c22315c8a2cd5310dc
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-07-20 22:25:38 +0900

    controller create 구현

--- src/create/CreateTodo.tsx
+++ src/create/CreateTodo.tsx
@@ -1,15 +1,30 @@
 import React, { useState } from "react";
 import type { DatePickerProps } from "antd";
-import { DatePicker, Space, Input, Form } from "antd";
+import { DatePicker, Space, Input, Form, Button } from "antd";
 import "../css/CreateTodo.css";
+import axios from "axios";
 
 const CreateTodo = () => {
   const { TextArea } = Input;
 
+  // const axios = require("axios").default;
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
-
+  const [todo, setTodo] = useState<string>("");
   const onChange: DatePickerProps["onChange"] = (date, dateString) => {
     console.log(date);
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTodo(e.target.value);
+  };
+
+  const createTodo = () => {
+    const input = {
+      do: todo,
+    };
+    axios.post("/todo", input).then((res) => {
+      console.log(res.data);
+    });
   };
 
   return (
@@ -24,8 +39,9 @@ const CreateTodo = () => {
       <Space direction="vertical">
         <div className="textTodo">
           <Form.Item label="내용">
-            <TextArea rows={4} />
+            <TextArea rows={4} onChange={handleChange} />
           </Form.Item>
+          <Button onClick={createTodo}>생성</Button>
         </div>
       </Space>
     </>
