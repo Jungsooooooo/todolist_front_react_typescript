c44cd7cbd4864bf8ae0575a1a81abc3ac9c05cd7
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-07-26 23:03:53 +0900

    해당 월에 할일들을 캘린더에다가 나타내기

--- src/calendar/CalendarHome.tsx
+++ src/calendar/CalendarHome.tsx
@@ -5,7 +5,6 @@ import type { CalendarMode } from "antd/es/calendar/generateCalendar";
 import type { BadgeProps } from "antd";
 import type { CellRenderInfo } from "rc-picker/lib/interface";
 import { UUID } from "crypto";
-import moment from "moment";
 import axios from "axios";
 
 interface Todo {
@@ -14,22 +13,19 @@ interface Todo {
   startDate: Date;
 }
 
+interface listDate {
+  type: string;
+  content: string;
+}
+
 const CalendarHome = () => {
-  const [todoData, setTodoData] = useState<Todo[]>([null]);
+  const [todoData, setTodoData] = useState<Todo[] | undefined>([]);
 
   useEffect(() => {
     getTodo();
   }, []);
 
-  const onPanelChange = (value: Dayjs, mode: CalendarMode) => {
-    console.log(value.format("YYYY-MM-DD"), mode);
-  };
-
   const getTodo = async () => {
-    //  return await axios.get("/todo/all").then((res) => {
-    //   setTodoData(res.data);
-    //   console.log(res.data);
-    // });
     const response = await axios.get("/todo/all");
     const todoData: Todo[] = response.data;
     setTodoData(todoData);
@@ -37,59 +33,34 @@ const CalendarHome = () => {
   };
 
   const getListData = (value: Dayjs) => {
-    let listData;
-    console.log(todoData[0].startDate);
-    switch (value.date()) {
-      case 8:
-        listData = [
-          { type: "warning", content: "This is warning event." },
-          { type: "success", content: "This is usual event." },
-        ];
-        break;
-      case 10:
-        listData = [
-          { type: "warning", content: "This is warning event." },
-          { type: "success", content: "This is usual event." },
-          { type: "error", content: "This is error event." },
-        ];
-        break;
-      case 15:
-        listData = [
-          { type: "warning", content: "This is warning event" },
-          { type: "success", content: "This is very long usual event。。...." },
-          { type: "error", content: "This is error event 1." },
-          { type: "error", content: "This is error event 2." },
-          { type: "error", content: "This is error event 3." },
-          { type: "error", content: "This is error event 4." },
-        ];
-        break;
-      default:
-    }
+    let listData: listDate[] = [];
+
+    todoData?.map((todo) => {
+      let fulldate = new Date(todo.startDate);
+      let month = fulldate.getMonth();
+      let date = fulldate.getDate();
+      console.log({ month });
+      console.log({ date });
+
+      if (value.month() === month) {
+        switch (value.date()) {
+          case date:
+            listData.push({ type: "success", content: todo.todo });
+            break;
+          default:
+        }
+      }
+    });
+
     return listData || [];
-  };
-
-  const getMonthData = (value: Dayjs) => {
-    if (value.month() === 8) {
-      return 1394;
-    }
-  };
-
-  const monthCellRender = (value: Dayjs) => {
-    const num = getMonthData(value);
-    return num ? (
-      <div className="notes-month">
-        <section>{num}</section>
-        <span>Backlog number</span>
-      </div>
-    ) : null;
   };
 
   const dateCellRender = (value: Dayjs) => {
     const listData = getListData(value);
     return (
       <ul className="events">
-        {listData.map((item) => (
-          <li key={item.content}>
+        {listData.map((item: listDate) => (
+          <li className="no_dot" key={item.content}>
             <Badge status={item.type as BadgeProps["status"]} text={item.content} />
           </li>
         ))}
@@ -99,7 +70,7 @@ const CalendarHome = () => {
 
   const cellRender = (current: Dayjs, info: CellRenderInfo<Dayjs>) => {
     if (info.type === "date") return dateCellRender(current);
-    if (info.type === "month") return monthCellRender(current);
+
     return info.originNode;
   };
 
