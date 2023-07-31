8ee77be967f8800ec1377da01df846b8112c0a43
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-07-31 22:43:10 +0900

    테이블 주소 입력

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Home from "./home/Home";
 import CreateTodo from "./create/CreateTodo";
+import TodoTable from "./table/TodoTable";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<CreateTodo />} />
+          <Route path="/table" element={<TodoTable />} />
         </Routes>
       </BrowserRouter>
     </div>
