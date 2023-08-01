4647c26b58f98ee49f4b16c3bbcf9e83811463d4
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-08-01 21:56:23 +0900

    테이블 형태로 볼 수 있게 antd 테이블 하면 구현

--- src/table/TodoTable.tsx
+++ src/table/TodoTable.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { Divider, Radio, Table } from "antd";
+import { Dayjs } from "dayjs";
 
 import axios from "axios";
 import { UUID } from "crypto";
@@ -7,23 +9,65 @@ interface Todo {
   key: UUID;
   todo: string;
   startDate: Date;
+  uid: UUID;
 }
+const columns = [
+  {
+    title: "할 일",
+    dataIndex: "todo",
+    key: "todo",
+  },
+  {
+    title: "시작일",
+    dataIndex: "startDate",
+    key: "startDate",
+  },
+  {
+    title: "종료일",
+    dataIndex: "endDate",
+    key: "endDate",
+  },
+];
+
+const rowSelection = {
+  onChange: (selectedRowKeys: React.Key[], selectedRows: Todo[]) => {
+    console.log(`selectedRowKeys: ${selectedRowKeys}`, "selectedRows: ", selectedRows);
+  },
+};
 
 const TodoTable = () => {
   const [allData, setAllData] = useState<Todo[] | undefined>([]);
+  const [dataSource, setDataSource] = useState<Todo[] | undefined>([]);
+  const [selectionType, setSelectionType] = useState<"radio">("radio");
 
   const getData = async () => {
     return await axios.get("/todo/all").then((res) => {
       console.log(res.data);
-      setAllData(res.data);
+      let datas = res.data;
+      datas.map((data: Todo) => {
+        data.key = data.uid;
+      });
+      setAllData(datas);
     });
   };
 
   useEffect(() => {
     getData();
-  });
+  }, []);
 
-  return <div>table</div>;
+  return (
+    <div>
+      <Divider />
+      <Table
+        rowSelection={{
+          type: selectionType,
+          ...rowSelection,
+        }}
+        dataSource={allData}
+        columns={columns}
+      />
+    </div>
+  );
 };
 
 export default TodoTable;
