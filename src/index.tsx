c23b99c6ea15f76ac94b0991ff8052dec3179324
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-08-03 20:17:36 +0900

    index Provider 적용

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import rootReducer from "./reducer";
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore } from "redux";
+
+const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
