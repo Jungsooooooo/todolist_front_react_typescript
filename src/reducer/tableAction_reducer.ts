e848abf87ce923565fcf31bd7c0d87507c03bce3
DESKTOP-LFV1CN5\USER <tnrlaos@gmail.com>
2023-08-05 16:19:16 +0900

    콘솔제거

--- src/reducer/tableAction_reducer.ts
+++ src/reducer/tableAction_reducer.ts
@@ -1,32 +1,24 @@
 import { CallTableInfo } from "../action/ttypes";
 import { bringYearAndMonthTable } from "../action/tAction";
 
-type YearAndMonth ={
-    year:any
-    month:any
-}
+type YearAndMonth = {
+  year: any;
+  month: any;
+};
 
-const initialState={
-    year:null,
-    month:null
-}
+const initialState = {
+  year: null,
+  month: null,
+};
 
 type Actiontype = ReturnType<typeof bringYearAndMonthTable>;
 
+export default function callTableReducer(state: YearAndMonth = initialState, action: Actiontype) {
+  switch (action.type) {
+    case CallTableInfo:
+      return { ...state, year: action.payload.year, month: action.payload.month };
 
-export default function callTableReducer(
-    state: YearAndMonth = initialState,
-    action: Actiontype
-){
-    console.log({action});
-    console.log({state});
-    switch(action.type){
-        
-        case CallTableInfo:
-        return {...state, year:action.payload.year,month:action.payload.month};
-
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 }
-
