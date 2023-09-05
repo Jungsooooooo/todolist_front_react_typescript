caffac75467e02cd16c3e763fdd98713067e7b4c
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2023-09-05 23:34:50 +0900

    로그인 화면 등록

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Home from "./home/Home";
 import CreateTodo from "./create/CreateTodo";
 import TodoTable from "./table/TodoTable";
+import Login from "./login/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<CreateTodo />} />
           <Route path="/table" element={<TodoTable />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </div>
