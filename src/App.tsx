83fbbe4887a517623508b489521d894ff530c05a
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2023-09-13 15:55:23 +0900

    route 권한 설정 완료

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,39 @@ import CreateTodo from "./create/CreateTodo";
 import TodoTable from "./table/TodoTable";
 import Login from "./login/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { getCookie } from "./cookie/Cookie";
+import PrivateRoute from "./PrivateRoute";
 
-function App() {
+const App = () => {
+  const cookie = getCookie("token");
+  console.log({ cookie });
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/create" element={<CreateTodo />} />
-          <Route path="/table" element={<TodoTable />} />
           <Route path="/login" element={<Login />} />
+          <Route
+            path="/"
+            element={
+              <PrivateRoute authenticated={cookie} component={<Home />} />
+            }
+          />
+          <Route
+            path="/create"
+            element={
+              <PrivateRoute authenticated={cookie} component={<CreateTodo />} />
+            }
+          />
+          <Route
+            path="/table"
+            element={
+              <PrivateRoute authenticated={cookie} component={<TodoTable />} />
+            }
+          />
         </Routes>
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
