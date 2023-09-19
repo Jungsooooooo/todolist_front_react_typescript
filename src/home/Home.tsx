2bd0b564d51137ad08a99322ea75e985ad108edb
DESKTOP-7L8OMEK\hr <tnrlaos@gmail.com>
2023-09-19 10:59:40 +0900

    로그아웃 기능 추가

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,10 +1,12 @@
-import { Button } from "antd";
+import { Button, Modal } from "antd";
 
 import CalendarHome from "../calendar/CalendarHome";
-
+import { getCookie } from "../cookie/Cookie";
 import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const { confirm } = Modal;
+
   const navigate = useNavigate();
 
   const goToCreate = () => {
@@ -15,12 +17,34 @@ const Home = () => {
     navigate("/table");
   };
 
+  const logout = () => {
+    confirm({
+      content: "로그아웃 하시겠습니까?",
+      onOk() {
+        document.cookie =
+          "token" + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        navigate("/login");
+        Modal.info({
+          content: (
+            <div>
+              <p>로그아웃 되었습니다.</p>
+            </div>
+          ),
+          onOk() {},
+        });
+      },
+    });
+  };
+
   return (
     <div>
       <Button type="primary" onClick={goToCreate}>
         할 일 적기
       </Button>
       <Button onClick={goToTable}>테이블로 보기</Button>
+      <Button danger type="text" className="logoutbutton" onClick={logout}>
+        로그아웃
+      </Button>
       <CalendarHome />
     </div>
   );
